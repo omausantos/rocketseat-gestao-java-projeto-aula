dea9aab90896346e0bfdb2316f7d9dee9797c62b
Mauricio Santos <mauwebjc@gmail.com>
2025-11-02 12:38:43 -0300

    feat(CandidateController) inserir validação de campos e dependencia validation

--- src/main/java/br/com/omausantos/gestao_vagas/modules/Candidate/CandidateEntity.java
+++ src/main/java/br/com/omausantos/gestao_vagas/modules/Candidate/CandidateEntity.java
@@ -3,6 +3,10 @@ package br.com.omausantos.gestao_vagas.modules.Candidate;
 import java.io.File;
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -10,8 +14,14 @@ public class CandidateEntity {
     
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^[^\\s]+$", message = "Campo USERNAME não deve conter espaços")
     private String username;
+
+    @Email(message = "Campo E-MAIL deve conter um e-mail válido")
     private String email;
+
+    @Length(min = 3, max = 15, message = "Campo SENHA deve conter entre 3 e 15 caracteres")
     private String password;
     private String description;
     private File curriculum;
