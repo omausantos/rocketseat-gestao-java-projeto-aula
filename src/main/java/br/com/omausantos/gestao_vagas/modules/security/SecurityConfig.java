c5509d77b959bd6a639c8ca8e4be10e71414b275
Mauricio Santos <mauwebjc@gmail.com>
2025-11-11 21:05:41 -0300

    feat(Security): Adicionando regra por URL

--- src/main/java/br/com/omausantos/gestao_vagas/modules/security/SecurityConfig.java
+++ src/main/java/br/com/omausantos/gestao_vagas/modules/security/SecurityConfig.java
@@ -9,8 +9,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.csrf(csrf -> csrf.disable());
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/candidate/").permitAll()
+                            .requestMatchers("/company/").permitAll();
+                    auth.anyRequest().authenticated();
+                });
         return http.build();
     }
 
